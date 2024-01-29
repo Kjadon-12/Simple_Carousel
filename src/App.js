486c4acc8558da7bfa6c35e9c882dd79c68b4a95
Kanchan Jadon <kanchanjadon96@gmail.com>
2024-01-29 21:25:27 +0530

    carousel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Carousel from "./Carousel";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Carousel />
+    </div>
+  );
+}
